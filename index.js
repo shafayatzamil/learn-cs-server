b3f6adf4a9151c98ba655677158bbcee00159506
shafayat zamil <zamilkhan111.za@gmail.com>
2022-10-25 18:15:18 +0600

    couse data plan

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
-const port = 5000
+const port = 5000;
+
+app.use(cors())
+
+const courses= require('./data/course.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/courses',(req,res)=>{
+    res.send(courses);
 })
 
 app.listen(port, () => {
