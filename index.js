ee3a789b4dabdb9a034e44d93546c99c1a1d4bf1
shafayat zamil <zamilkhan111.za@gmail.com>
2022-10-25 19:48:01 +0600

    single item courses updated

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ const port = 5000;
 
 app.use(cors())
 
-const courses= require('./data/course.json');
+const courses= require('./data/course-module.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/courses',(req,res)=>{
     res.send(courses);
+})
+
+app.get('/courses/:id',(req,res)=>{
+    const id = req.params.id;
+    const getSingleCouses = courses?.find((p) => p.id == id);
+    console.log(getSingleCouses);
+    // if (!getSingleItem) {
+    //   res.send("Porduct khuje pai nai");
+    // }
+    res.send(getSingleItem);
 })
 
 app.listen(port, () => {
