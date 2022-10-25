f8effc664ad5b11ac3f7d6e724c0e3436c2a20d7
shafayat zamil <zamilkhan111.za@gmail.com>
2022-10-26 01:23:44 +0600

    catagory data updated

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
+const express = require("express");
+var cors = require("cors");
+const app = express();
 const port = 5000;
 
-app.use(cors())
+app.use(cors());
 
-const courses= require('./data/course-module.json');
+const courses = require("./data/course-module.json");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/courses',(req,res)=>{
-    res.send(courses);
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
-app.get('/courses/:id',(req,res)=>{
-    const id = req.params.id;
-    const getSingleCouses = courses?.find((p) => p.id == id);
-    console.log(getSingleCouses);
-    // if (!getSingleItem) {
-    //   res.send("Porduct khuje pai nai");
-    // }
-    res.send(getSingleItem);
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const getSingleCourses = courses?.find((p) => p.id == id);
+  if (!getSingleCourses) {
+    res.send("Porduct khuje pai nai");
+  }
+  res.send(getSingleCourses);
+});
+
+app.get("/category/:name",(req,res)=>{
+  const name =req.params.name;
+  console.log(name);
+  const getCategory = courses?.filter((p) => p.category == name);
+  res.send(getCategory); 
+
+  
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
